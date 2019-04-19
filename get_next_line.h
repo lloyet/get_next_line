d69ee912c18d1e4919a11a0b3c4adf65104f370a
Louis Loyet <lloyet@z4r5p3.le-101.fr>
2019-04-19 02:57:55 +0200

    new gnl

---
+++ get_next_line.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                          LE - /            */
+/*                                                              /             */
+/*   get_next_line.h                                  .::    .:/ .      .::   */
+/*                                                 +:+:+   +:    +:  +:+:+    */
+/*   By: lloyet <marvin@le-101.fr>                  +:+   +:    +:    +:+     */
+/*                                                 #+#   #+    #+    #+#      */
+/*   Created: 2018/08/27 15:20:04 by lloyet       #+#   ##    ##    #+#       */
+/*   Updated: 2018/09/01 15:59:07 by lloyet      ###    #+. /#+    ###.fr     */
+/*                                                         /                  */
+/*                                                        /                   */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include "libft/libft.h"
+
+# define BUFF_SIZE	1
+
+int			get_next_line(const int fd, char **line);
+#endif
